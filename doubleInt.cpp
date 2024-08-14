97f43f051c798eecd11bbd2161e3e5e158f21c77
juan miguel jimenez <76181462+Jjimenez212@users.noreply.github.com>
2024-08-14 12:15:59 -0700

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,20 @@
+// JJ 8/14/2024
+// doubleAnInt.cpp
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userInteger = 0;
+    int doubleInteger = 0;
+    cout << "Please enter a number: ";
+    cin >> userInteger;
+
+    // Do Some Processing
+    doubleInteger = userInteger * 2;
+
+    //Output the double int
+    cout << "\n The double int is:" << doubleInteger << endl;
+
+}
