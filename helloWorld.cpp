d4a6a69839b6682e077c5b722cf7118e0c71b79d
juan miguel jimenez <76181462+Jjimenez212@users.noreply.github.com>
2024-08-14 11:11:09 -0700

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// JJ 8/14/24
+// Juan Miguel Jimenez
+// helloWorld
+/*
+ *this is my first c++ program
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nHello, World! " << " another string" << endl;
+    return 0;
+}
